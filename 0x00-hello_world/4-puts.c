f1f06d28e0806b7e84521d43603e1f837de8004d
chiagoziem11 <chiagoziem11@yahoo.com>
2022-10-13 03:37:39 -0700

    modified 4-puta.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ *main - Entry poin
  *
- * Return always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
+puts("\"Programming is like building a multilingual puzzle\n");
 
+return (0);
+}
