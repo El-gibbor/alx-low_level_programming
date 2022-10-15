18b64a380055652b8c709d2e483d21aae3fdc2c0
chiagoziem11 <chiagoziem11@yahoo.com>
2022-10-15 01:24:17 -0700

    a program that prints all possible different combinations of two digits. seperetated by comma followed by spaces, two digits must be different

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,18 +4,21 @@
  * main - Entry point
  * print all possiblecombinations of single digit
  */
+
 int main(void)
 {
-	int number;
+		int number;
 
-	for (number = 0; number <= 9; number++)
-	{
-	putchar(number + '0');
-	if (number < 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
+		for (number = 0; number <= 9; number++)
+		{
+		putchar(number + '0');
+		if (number < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		putchar('\n');
+
+		return (0);
 }
