8c912659a62cf847efb99e17532deb6d70aa8ae7
chiagoziem11 <chiagoziem11@yahoo.com>
2022-10-18 06:57:16 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-
 /**
- * main -  prints _putchar
+ * main - prints _putchar
  * written by Chiagoziem Elgibbor
  * Return: Always 0 (success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
