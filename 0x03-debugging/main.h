42bbe7c2847c0d9ae8a4a69ef52ff8d6b2232eb0
chiagoziem11 <chiagoziem11@yahoo.com>
2022-10-20 04:53:34 -0700

    fixed the print_remaining_days() function so that the output works correctly for all dates and all leap years.

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-int print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
