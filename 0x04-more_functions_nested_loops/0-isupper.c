9ddfa560721053a935967d7a665074b1da85fdb8
chiagoziem11 <chiagoziem11@yahoo.com>
2022-10-21 20:35:34 -0700

    a function that checks for uppercase character,returns 1 if true or 0 if otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,16 @@
 /**
  * _isupper - checkes for uppercase
  * @c: parameter to be checked
- * Return: 1 if true, else o if otherwise.
+ *
+ * Return: 1 if true. else, 0 if otherwise.
  */
 
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
 		return (0);
 	else
 		return (1);
+
 }
