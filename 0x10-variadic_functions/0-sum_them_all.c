7fb7aee9f089ae0576dff669c04569bbf0cc69c6
chiagoziem11 <chiagoziem11@yahoo.com>
2022-11-26 01:08:52 -0800

    returns the sum of all its variadic arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * sum_them_all - is a function that returns the sum of all its parameters
+ * sum_them_all - returns the sum of all its parameters
  * @n: is the integer parameter
  * Return: sum or if n == 0, Return 0.
  */
