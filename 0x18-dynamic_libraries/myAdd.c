292aa22e98fe58623c30d247e1e02185264ba839
chiagoziem11 <chiagoziem11@yahoo.com>
2023-01-19 08:33:27 -0800

    function that adds two digits

---
+++ 0x18-dynamic_libraries/myAdd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * add - function that adds two values
+ * @a: paremeter to add
+ * @b: paremeter to add
+ * Return: 0 (succes)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
