7e3a445b41ec0a143c4443fb64144608279cf512
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-01-20 16:32:34 +0400

    Update quick-push.sh

--- quick-push.sh
+++ quick-push.sh
@@ -1,13 +1,9 @@
 # a script that authomatically push my work to remote (github)
 
 #!/bin/bash
-
-echo "what is your commit message"
-
+# script that adds all files, commit and push to github
+echo "please input your commit message"
 read MESSAGE
-
 git add .
-
 git commit -m "$MESSAGE"
-
 git push
