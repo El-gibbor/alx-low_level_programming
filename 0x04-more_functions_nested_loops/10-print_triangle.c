dfffe6236949a7123cfb99599af614b15828420f
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-05-18 19:55:04 +0400

    more relatable and clear method for diagonal printing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,3 +18,24 @@ void print_triangle(int size)
 		_putchar('\n');
 	}
 }
+
+// void print_triangle(int size)
+// {
+//     int i, j;
+
+//     if (size <= 0)
+//         putchar('\n');
+//         return;
+        
+//     for (i = 1; i <= size; i++)
+//     {
+//         for (j = 1; j <= size; j++)
+//         {
+//             if (j <= size - i)
+//                 putchar(' ');
+//             else
+//                 putchar('#');
+//         }
+//         putchar('\n');
+//     }
+// }
