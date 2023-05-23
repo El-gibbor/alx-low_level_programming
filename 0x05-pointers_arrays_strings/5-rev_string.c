516113ba2fed473534ee0046e1c05f3ea971630e
El-gibbor <chiagoziem11@yahoo.com>
2023-05-23 20:56:47 +0400

    improved and more readable

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,39 +6,22 @@
  * Return: void
  * Authur: ELGIBBOR
  */
-// void rev_string(char *s)
-// {
-// 	int i, len;
-
-// 	/* get the sring legnth */
-// 	for (len = 0; s[len] != '\0'; len++)
-// 	{
-// 		;
-// 	}
-// 	/* swap indx 0 with last indx progresively till the midle indx */
-// 	for (i = 0; i < len / 2; i++)
-// 	{
-// 		char swap_var;
-
-// 		swap_var = s[i];
-// 		s[i] = s[len - 1 - i];
-// 		s[len - 1 - i] = swap_var;
-// 	}
-// }
 void rev_string(char *s)
-	{
-	int a = 0, b, c;
-	char d;
+{
+	int i, len;
 
-	while (s[a] != '\0')
+	/* get the sring legnth */
+	for (len = 0; s[len] != '\0'; len++)
 	{
-		a++;
+		;
 	}
-	c = a - 1;
-	for (b = 0; c >= 0 && b < c; c--, b++)
+	/* swap indx 0 with last indx progresively till the midle indx */
+	for (i = 0; i < len / 2; i++)
 	{
-		d = s[b];
-		s[b] = s[c];
-		s[c] = d;
+		char swap_var;
+
+		swap_var = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = swap_var;
 	}
 }
