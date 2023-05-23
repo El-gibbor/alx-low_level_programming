7daf2490a89153325fdf33a80838a67640263f97
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-05-23 21:28:06 +0400

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,17 +5,6 @@
  * @str: paraameter to print
  * Return: void
  */
-
-// void puts2(char *str)
-// {
-// 	int char_val;
-
-// 	for (char_val = 0; str[char_val] != '\0'; char_val++)
-// 	if (char_val % 2 == 0)
-// 		_putchar(str[char_val]);
-// 	_putchar('\n');
-// }
-
 void puts2(char *str)
 {
 	int char_val;
