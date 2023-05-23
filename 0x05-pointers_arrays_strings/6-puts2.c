c4c653603dd0b635baa2f940b891ce59f5e2ae33
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-05-23 21:31:23 +0400

    improved alternative

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,18 @@ void puts2(char *str)
 {
 	int char_val;
 
-	for (char_val = 0; str[char_val] != '\0'; char_val += 2)
+	for (char_val = 0; str[char_val] != '\0'; char_val++)
+	if (char_val % 2 == 0)
 		_putchar(str[char_val]);
 	_putchar('\n');
 }
+
+// void puts2(char *str)
+// {
+// 	int char_val;
+	
+// 	/* access values 1 byte after each other in memory */
+// 	for (char_val = 0; str[char_val] != '\0'; char_val += 2)
+// 		_putchar(str[char_val]);
+// 	_putchar('\n');
+// }
