56f46154eef313ef18e36ebafa91f3d606c81d9f
El-gibbor <chiagoziem11@yahoo.com>
2023-05-31 20:36:28 +0400

    reimplemented strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,19 @@
 #include "main.h"
 /**
- * _strcmp - compare 2 string
- * @s1:string
- * @s2:strmp
- * Return:int
- * Bwave/ Bright
+ * _strcmp - compares 2 strings
+ * @s1: first string to be compared with
+ * @s2: second string compared on/with first string
+ * Return: 0 if equal, positive if s1 < s2, negative if s1 > s2
+ * Author: ELGIBBOR
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int i = 0;
 
-	if (*s1 == *s2)
+	for (; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		return (0);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (0);
 }
