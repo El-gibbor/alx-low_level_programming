a42bf5da13d8eef11b1b56159c37c5e73664e701
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-05-31 12:19:53 +0400

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,9 @@
 
 /**
  *_strncpy - function that copies a string
- *
  * @src: The source of strings to be copied
  * @dest: The destination of the string
  * @n: The number of characters to be copied
- *
  * Return: pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
