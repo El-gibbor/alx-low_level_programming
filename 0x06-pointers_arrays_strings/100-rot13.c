51ad119bb802c80484dc8bfb85c3f51e245da225
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-06-02 17:19:01 +0400

    added and alternative implementation

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,7 +3,6 @@
 /**
  * rot13 - encodes a string into rot13
  * @s: string to encode
- *
  * Return: address of s
  */
 char *rot13(char *s)
@@ -25,3 +24,18 @@ char *rot13(char *s)
 	}
 	return (s);
 }
+
+// char *rot13(char *s)
+// {
+// 	int i;
+
+// 	for (i = 0; s[i]; i++)
+// 	{
+// 		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+// 			s[i] += 13;
+		
+// 		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+// 			s[i] -= 13;
+// 	}
+// 	return s;
+// }
