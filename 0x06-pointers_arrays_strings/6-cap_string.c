1d4717be7854039c2e1894d5acfa0ebc88ff5ac5
El-gibbor <chiagoziem11@yahoo.com>
2023-06-02 16:45:27 +0400

    reimplemented string-upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,28 +4,24 @@
  * cap_string - capitalizes all words in a string
  * @s: string
  * Return: address of s
+ * Author: ELGIBBOR
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	while (*(s + i))
+	/* handle first character in the string */
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		for (j = 0; sep[j] != '\0'; j++)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
+			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
 		}
-		i++;
 	}
 	return (s);
 }
