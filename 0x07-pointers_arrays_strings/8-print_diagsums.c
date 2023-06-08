9856179d334af8bce74498bb71bbc9bb81cd5b26
El-gibbor <chiagoziem11@yahoo.com>
2023-06-08 20:09:34 +0400

    refactored diagonal sum func

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,18 +9,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int row, col, sumDiag = 0, sumDiag1 = 0;
 
-	for (i = 0; i < size; i++)
+	for (row = 0; row < size; row++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		for (col = 0; col < size; col++)
+		{
+			if (row == col)
+				sumDiag += a[row * size + col];
+
+			if (row + col == (size - 1))
+				sumDiag1 += a[row * size + col];
+		}
 	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
-	}
-	printf("%i, %i\n", l, r);
+	printf("%d, %d\n", sumDiag, sumDiag1);
 }
-
