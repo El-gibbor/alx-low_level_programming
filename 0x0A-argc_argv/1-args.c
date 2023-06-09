0ab3a4c2eb2f0cca558c641416a31d47ce2b21c4
El-gibbor <chiagoziem11@yahoo.com>
2023-06-09 20:29:12 +0400

    prints number of args - implemented & compiled unused var

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: arguments count
+ * @argv: Vector - array of strings (the actual arguments)
+ * Return: 0 (success)
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
