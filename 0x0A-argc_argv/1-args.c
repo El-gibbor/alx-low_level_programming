b5a15f90d3067086d5475fa11b0c11466638889a
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-06-09 20:44:28 +0400

    number of arguments and bypassing compiler warning for unused variable

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,6 +8,12 @@
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	/**
+	* the __attribute__((unused)) placed after the declaration
+	* of the paremeter informs the compiler that the variable is
+	* intentionally left unused. By using this attribute, the compiler
+	* is prevented from generating warnings for unused variables within the function.
+	*/
 	printf("%d\n", argc - 1);
 	return (0);
 }
