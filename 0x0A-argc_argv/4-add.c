743006371f732c7541bd45f12b65672851a91744
El-gibbor <chiagoziem11@yahoo.com>
2023-06-10 17:13:09 +0400

    program that prints sum of cmdline args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of cmdline args
+ * @argc: argument count
+ * @argv: cmdline arguments passed
+ * Return: 1 if arguments contain values that are not digit
+ */
+int main(int argc, char *argv[])
+{
+	int indx, sumOfArgs = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (indx = 1; indx < argc; indx++)
+		{
+			if (*argv[indx] > '0' && *argv[indx] < '9')
+			{
+				sumOfArgs += atoi(argv[indx]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sumOfArgs);
+	}
+	return (0);
+}
