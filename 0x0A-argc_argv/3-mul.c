36a08942326b8ac086728b308e7fc308dec043bf
El-gibbor <chiagoziem11@yahoo.com>
2023-06-10 11:40:15 +0400

    multiplies 2 cmdline arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers (cmdline args)
+ * @argc: argument count
+ * @argv: list of cmdline arguments to be multiplied
+ * Return: 1 if 2 arguments is not give
+ * Author: ELGIBBOR
+ */
+int main(int argc, char *argv[])
+{
+	int operand1, operand2;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	operand1 = atoi(argv[1]);
+	operand2 = atoi(argv[2]);
+	printf("%d\n", operand1 * operand2);
+
+}
