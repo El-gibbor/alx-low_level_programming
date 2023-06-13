fde9a3d1b8954c6f9f57a33fa7077b18d409aa01
El-gibbor <chiagoziem11@yahoo.com>
2023-06-13 14:21:06 +0400

    allocate & return pointer to a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - allocate & return memory for a string duplicate
+ * @str: string to return a memory to its duplicate
+ * Return: memory to the string duplicate
+ * Author: Elgibbor
+*/
+char *_strdup(char *str)
+{
+	char *ptr_alloc, *ptr_alloc_char;
+	int strLen = _strlen(str);
+
+	ptr_alloc = malloc(sizeof(char) * strLen + 1);
+	if (str != NULL)
+	{
+	/* hold base address with return ptr & perform duplicate with another ptr */
+		ptr_alloc_char = ptr_alloc;
+		while (*str)
+		{
+			*ptr_alloc_char = *str;
+			str++;
+			ptr_alloc_char++;
+		}
+		*ptr_alloc_char = '\0';
+	}  return ptr_alloc;
+}
