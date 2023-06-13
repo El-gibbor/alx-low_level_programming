3f332cbcc462d93428d43adcfb00cb9a3c0cd697
El-gibbor <chiagoziem11@yahoo.com>
2023-06-13 15:04:47 +0400

    compiled without an archive to satisfy checker

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,23 +11,24 @@
 char *_strdup(char *str)
 {
 	char *ptr_alloc, *ptr_alloc_char;
-	/* int strLen = _strlen(str); */
-	size_t strLen = 0;
+	/* int len = _strlen(str); */
+	size_t len;
 
-	for (; str[strLen]; strLen++)
+	for (len = 0; str[len]; len++)
 		;
 
-	ptr_alloc = malloc(sizeof(char) * strLen + 1);
-	if (str != NULL)
+	ptr_alloc = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
 		return (NULL);
+
 	/* hold base address with return ptr & perform duplicate with another ptr */
-		ptr_alloc_char = ptr_alloc;
-		while (*str)
-		{
-			*ptr_alloc_char = *str;
-			str++;
-			ptr_alloc_char++;
-		}
-		*ptr_alloc_char = '\0';
-		return (ptr_alloc);
+	ptr_alloc_char = ptr_alloc;
+	while (*str)
+	{
+		*ptr_alloc_char = *str;
+		str++;
+		ptr_alloc_char++;
+	}
+	*ptr_alloc_char = '\0';
+	return (ptr_alloc);
 }
