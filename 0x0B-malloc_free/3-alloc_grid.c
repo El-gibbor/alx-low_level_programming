25b80507e9d2b5e1498d1d05d764d432ebc6e0c1
El-gibbor <chiagoziem11@yahoo.com>
2023-06-14 08:58:02 +0400

    allocate & return a ptr to 2D array of int initialised with 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocate & return a ptr to 2D array of int initialised with 0
+ * @width: rows of the 2D array
+ * @height: columns of the 2D array
+ * Return: rows base address pointing to address(ptr) of each column in a row
+ * Author: Elgibbor
+*/
+int **alloc_grid(int width, int height)
+{
+	int **ptr2D, rowIndx, colIndx, row;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr2D = malloc(sizeof(int *) * height);
+	if (!ptr2D)
+		return (NULL);
+	for (row = 0; row < height; row++)
+	{
+		ptr2D[row] = malloc(sizeof(int) * width);
+	}
+	for (rowIndx = 0; rowIndx < height; rowIndx++)
+	{
+		for (colIndx = 0; colIndx < width; colIndx++)
+			ptr2D[rowIndx][colIndx] = 0;
+	}
+	return (ptr2D);
+
+	for (row = 0; row < height; row++)
+		free(ptr2D[row]);
+
+	free(ptr2D);
+}
