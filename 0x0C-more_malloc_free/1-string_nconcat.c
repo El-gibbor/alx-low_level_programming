2488f03f0def2eaa69e7507e24f94c6d3b3104ae
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-06-15 17:30:34 +0400

    modified to handle when string arguments are NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str_alloc;
 	char *charInStr;
 
+	/* Handle parameters as empty string when they are NULL*/
 	if (!s1)
 		s1 = "";
 	if (!s2)
@@ -23,21 +24,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n_byte < 0)
 		return (NULL);
 
+	/* get total length of the expected strings for concatination */
 	if (n_byte >= myStrlen(s2))
 		total_str = myStrlen(s1) + myStrlen(s2);
 	else
 		total_str = myStrlen(s1) + n_byte;
-
+	
+	/* allocate memory for concatination and handle malloc failure */
 	str_alloc = malloc(total_str + 1);
 	if (!str_alloc)
 		return (NULL);
-
+	
+	/* hold base address with return ptr & perform concatination with another ptr */
 	charInStr = str_alloc;
 	for (strIndx = 0; s1[strIndx] != '\0'; strIndx++)
 	{
 		*charInStr = s1[strIndx];
 		charInStr++;
 	}
+	/* concatinate n byte of str2 into the allocated memory*/
 	for (s2_str = 0; s2[s2_str] != '\0' && s2_str < n_byte; s2_str++)
 	{
 		*charInStr = s2[s2_str];
