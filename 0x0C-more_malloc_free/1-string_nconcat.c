deae5ad77c42ed3d8aaa04efb56e6925c2958a57
El-gibbor <chiagoziem11@yahoo.com>
2023-06-15 14:10:07 +0400

    concatenates two strings & return memory addrs of the concatination

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+#include "myStrlen.c"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string to be concatinated into a newly allocated space in memory
+ * @s2: concatinates n number of bytes from this string to the one from s1
+ * @n: number of bytes be taken from s2 and concatinate to s1
+ * Return: pointer to the allocated space in memory containing what is above
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int n_byte = n, total_str, s2_str, strIndx;
+	char *str_alloc;
+	char *charInStr;
+
+	if (!s1 || !s2)
+		return ("");
+
+	if (n_byte >= myStrlen(s2))
+		total_str = myStrlen(s1) + myStrlen(s2);
+	else
+		total_str = myStrlen(s1) + n_byte;
+
+	str_alloc = malloc(total_str + 1);
+	if (!str_alloc)
+		return (NULL);
+
+	charInStr = str_alloc;
+	for (strIndx = 0; s1[strIndx] != '\0'; strIndx++)
+	{
+		*charInStr = s1[strIndx];
+		charInStr++;
+	}
+	for (s2_str = 0; s2[s2_str] != '\0' && s2_str < n_byte; s2_str++)
+	{
+		*charInStr = s2[s2_str];
+		charInStr++;
+	}
+	*charInStr = '\0';
+	return (str_alloc);
+}
