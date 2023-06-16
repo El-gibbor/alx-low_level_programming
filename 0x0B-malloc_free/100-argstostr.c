913c7e9b401060d4f9069e351ecdb37afa6d8577
El-gibbor <chiagoziem11@yahoo.com>
2023-06-17 00:58:59 +0400

    concatinates cmdline args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - get the string length
+ * @a: the given string
+ * Return: lenghth of the string
+ */
+int _strlen(char *a)
+{
+	int len = 0;
+
+	for (; a[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+ * argstostr - cocatinates all arguments of a prog
+ * @ac: argument count
+ * @av: commandline arguments
+ * Return: pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, strLen;
+		char *cmdarg;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (; i < ac; i++)
+	{
+		strLen += _strlen(av[i]);
+	}
+	cmdarg = malloc(strLen + ac + 1);
+		if (!cmdarg)
+			return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			cmdarg[k] = av[i][j];
+			k++;
+		}
+		cmdarg[k] = '\n';
+		k++;
+	}
+	cmdarg[k] = '\0';
+	return (cmdarg);
+}
--- 0x0B-malloc_free/testFiles/main.h
+++ 0x0B-malloc_free/testFiles/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int **alloc_grid(int width, int height);
 int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
