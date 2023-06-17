d4746cb3fa92e35c62e8e542908fbdae350678cf
El-gibbor <chiagoziem11@yahoo.com>
2023-06-17 21:55:56 +0400

    handled mem leakage for previously allocated mem if malloc fails

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - memory allocation & splits a string into words
+ * @str: string to be splitted
+ * Return: a pointer to an array of strings.
+*/
+char **strtow(char *str)
+{
+    if (!str || str == "")
+        return (NULL);
+        
+
+}
--- 0x0B-malloc_free/testFiles/main.h
+++ 0x0B-malloc_free/testFiles/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 int **alloc_grid(int width, int height);
-int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int myStrLen(char *a);
+/*int myStrLen(char *a);*/
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif /* main.h */
