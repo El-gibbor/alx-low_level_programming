c8a4a7c78f63d22047cefde60b21ee2d4039f6f8
El-gibbor <chiagoziem11@yahoo.com>
2023-06-18 14:28:48 +0400

    to be continued

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints min number of coins to make change for an amount of money
+ * @argc: argument count
+ * @argv: cmdline args (amount of money passed)
+ * Return: 1 if number of arguments is not exactly 1
+*/
+int main(int argc, char *argv[])
+{
+    
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - creates an array of integers
+ * @min: 
+*/
+int *array_range(int min, int max);
