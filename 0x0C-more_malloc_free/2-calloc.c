9a25c720854aadc57b3caf86b8cba0d35a1b2d50
El-gibbor <chiagoziem11@yahoo.com>
2023-06-18 14:12:45 +0400

    added neline at the end to conform with betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-#include "funcFiles/myMemSet.c"
-#include <string.h>
 
 /**
  * _calloc - allocates memory to an array
@@ -11,16 +9,18 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-     void *arr_alloc;
-     unsigned int i;
+	 void *arr_alloc;
+	 unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    arr_alloc = malloc(nmemb * size);
-    if (!arr_alloc)
-        return (NULL);
-    for (i = 0; i < (nmemb * size); i++)
+	arr_alloc = malloc(nmemb * size);
+	if (!arr_alloc)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
 		((char *) (arr_alloc))[i] = 0;
-    return (arr_alloc);
+	
+	return (arr_alloc);
 }
