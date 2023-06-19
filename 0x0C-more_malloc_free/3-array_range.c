04dbd4bc4ffa5d76840674577387b537b4a8dfbd
El-gibbor <chiagoziem11@yahoo.com>
2023-06-19 10:08:21 +0400

    malloc for arrays of integers ranging for min - max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
 #include "main.h"
+#include "funcFiles/set_array.c"
+
 /**
  * array_range - creates an array of integers
- * @min: 
+ * @min: array values ranging to minimum
+ * @max: array values ranging to maximum
+ * Return: pointer to the newly created array
 */
-int *array_range(int min, int max);
+int *array_range(int min, int max)
+{
+	int arraySize, *array_alloc;
+
+	if (min > max)
+		return (NULL);
+
+	arraySize = (max - min) + 1;
+
+	array_alloc = malloc(arraySize * sizeof(int));
+	if (!array_alloc)
+		return (NULL);
+
+	set_array_val(array_alloc, min, max);
+	return (array_alloc);
+}
