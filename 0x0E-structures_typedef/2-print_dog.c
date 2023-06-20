0febfee0869c79ff1623adef5d3914e1651693ce
El-gibbor <chiagoziem11@yahoo.com>
2023-06-20 12:44:15 +0400

    modified float format for age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,15 @@ void print_dog(struct dog *d)
 		printf("(nil)");
 	if (!d->owner)
 		printf("(nil)");
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return (0);
 }
