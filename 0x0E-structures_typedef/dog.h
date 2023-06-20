5c99edc92f28fca4f581aae940821f0a09e5a880
El-gibbor <chiagoziem11@yahoo.com>
2023-06-20 08:47:23 +0400

    defined a struct dog with a few given elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog owner
+ * Description - None.
+*/
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
