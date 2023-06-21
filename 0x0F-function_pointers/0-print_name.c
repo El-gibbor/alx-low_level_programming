724a956658337483114b2be2231d233c72644aea
El-gibbor <chiagoziem11@yahoo.com>
2023-06-21 13:06:09 +0400

    header file containing all function prototypes

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: pointer to character (name to be printed)
+ * @f: pointer to function that prints name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    if (f && name)
+        (*f)(name);
+}
