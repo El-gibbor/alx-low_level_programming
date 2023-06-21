d360213e1cfa64e8e5a41d5deb57dfbc62883ce2
El-gibbor <chiagoziem11@yahoo.com>
2023-06-21 21:36:30 +0400

    callback with function pointer to a func that compares the integers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to search in
+ * @size: size of the array
+ * @cmp: ptr to the func to be used to compare values
+ * Return: Index of the first element from cmp after comparing
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
