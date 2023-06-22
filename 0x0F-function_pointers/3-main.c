ad07021d1456efa17602a1f221dd69658cfd51e6
El-gibbor <chiagoziem11@yahoo.com>
2023-06-22 20:31:28 +0400

    modified line spacing & wrong indentation to conform with betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -25,8 +26,8 @@ int main(int argc, char *argv[])
 	/*handle zero division and modulos*/
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
-			printf("Error\n");
-			exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
