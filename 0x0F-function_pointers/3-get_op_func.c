4b14c25e1f4d73ebf0d893173a6045def1ab971f
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-06-22 15:39:13 +0400

    fetches pointer to the correct func to perform operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	while (i < 6)
 	{
 		if (*s == ops[i].op) /*if entered char is equal to a a valid operator*/
-			return (ops[i].f);/*return corresponding func ptr to that operator*/
+			return (ops[i].f); /*return corresponding func ptr to that operator*/
 	}
-	return (NULL); /*NULL if s doesnt match the available 5 operators*/
+	return (NULL); /*NULL if s doesn't match the available 5 operators*/
 }
