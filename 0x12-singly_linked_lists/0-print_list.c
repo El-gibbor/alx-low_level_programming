3c6e8b787bbfc8917887f5332246d890a8154079
El-gibbor <chiagoziem11@yahoo.com>
2023-06-30 17:56:35 +0400

    mdoified var declaration to please requirement check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
-		if (!(*h).str)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", ((*h).len), (*h).str);
-		h = (*h).next;
-		i++;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
