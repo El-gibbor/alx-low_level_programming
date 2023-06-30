58b2c969f346e11c9cdd9f4975bd6ed9ec2d1ee5
El-gibbor <chiagoziem11@yahoo.com>
2023-06-30 17:15:50 +0400

    prints all elemnts and return the lenght element of the list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list to print
+ * Return: length member of the list_t list
+*/
+	size_t print_list(const list_t *h)
+	{
+		size_t i = 0;
+
+		for (; h; i++)
+		{
+			if (!(*h).str)
+				printf("[0] (nil)\n");
+			else
+				printf("[%d] %s\n", (*h).len, (*h).str);
+			h = (*h).next;
+		}
+		return (i);
+	}
