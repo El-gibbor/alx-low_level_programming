d722470a66b1d98b47b3dde6ec8731bdfdc8bd85
El-gibbor <chiagoziem11@yahoo.com>
2023-06-30 17:30:33 +0400

    mdoified var declaration to please requirement check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,11 +11,11 @@
 
 		for (i = 0; h != NULL; i++)
 		{
-			if (!(*h).str)
+			if (!h->str)
 				printf("[%d] %s\n", 0, "(nil)");
 			else
-				printf("[%d] %s\n", (*h).len, (*h).str);
-			h = (*h).next;
+				printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
 		}
 		return (i);
 	}
