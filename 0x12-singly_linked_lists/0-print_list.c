da13695f644cabd30e6dcbcbbb2a6e27ceeda157
El-gibbor <chiagoziem11@yahoo.com>
2023-06-30 21:24:08 +0400

    redtified betty in header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: ponter to the list to be printed
+ * Return: the number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t numNodes;
+
+	numNodes = 0;
+	while (h)
+	{
+		if (!(*h).str)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", ((*h).len), (*h).str);
+		h = (*h).next;
+		numNodes++;
+	}
+	return (numNodes);
+}
