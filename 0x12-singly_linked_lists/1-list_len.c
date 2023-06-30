c2be4e185d70d0b2c0dc21beefe49d96f7f62680
El-gibbor <chiagoziem11@yahoo.com>
2023-06-30 18:13:14 +0400

    prints the number of elements (nodes) in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - prints the number of elements in a linked list_t list
  * @h: ponter to the list
- * Return: the number of elements in a linked list_t list
+ * Return: the number of elements (nodes) in a linked list
 */
 size_t list_len(const list_t *h)
 {
