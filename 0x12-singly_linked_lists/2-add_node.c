55188ac322a93a1856755f05f7278866cfe98dc3
Chiagoziem Elgibbor <chiagoziem11@yahoo.com>
2023-07-01 18:13:17 +0400

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to the list to be printed
+ * @head: pointer to the address of the first node
  * @str: string to be added(duplicated) to the list
  * Return: the address of the new element, or NULL if it failed
  */
