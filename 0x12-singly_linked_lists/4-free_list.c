4f6fd4c64f2c133e3e09c9d3ba3332bd692a618a
El-gibbor <chiagoziem11@yahoo.com>
2023-07-01 20:36:22 +0400

    reindented spaces to tabs

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,16 +3,16 @@
 /**
  * free_list - frees/deallocate a single linked list
  * @head: pointer to the list to be freed
-*/
+ */
 void free_list(list_t *head)
 {
-    list_t *trv_ptr;
-    
-    while (head)
-    {
-        trv_ptr = head;
-        head = (*head).next;
-        free((*trv_ptr).str);
-        free(trv_ptr);
-    }
+	list_t *trv_ptr;
+
+	while (head)
+	{
+		trv_ptr = head;
+		head = (*head).next;
+		free((*trv_ptr).str);
+		free(trv_ptr);
+	}
 }
