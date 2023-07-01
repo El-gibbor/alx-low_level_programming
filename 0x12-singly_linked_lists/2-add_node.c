db93785714f9314e2c6235349c2bb7b9d4a738a3
El-gibbor <chiagoziem11@yahoo.com>
2023-07-01 17:56:41 +0400

    func that adds a new node at the begining of a l_list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,8 +5,32 @@
  * @head: pointer to the list to be printed
  * @str: string to be added(duplicated) to the list
  * Return: the address of the new element, or NULL if it failed
-*/
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-    
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	(*new_node).str = strdup(str);
+	(*new_node).len = _strlen(str);
+
+	(*new_node).next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * _strlen - returns the lenth of a string
+ * @s: Paremeter to count
+ * Return: length.
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	for (; s[len]; len++)
+		;
+	return (len);
 }
