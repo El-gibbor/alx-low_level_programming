1e75342a50c505986984a01540e07df1922d2143
El-gibbor <chiagoziem11@yahoo.com>
2023-07-01 22:11:20 +0400

    func that adds a newnode at the end of a l_list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a l_list
+ * @head: pointer to a pointer to the first node in the list
+ * @str: member of the struct node (data field)
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *last_node, *curr_node;
+
+    last_node = malloc(sizeof(list_t));
+    if (!last_node)
+        return (NULL);
+    
+    (*last_node).str = strdup(str);
+    (*last_node).len = _strlen(str);
+    (*last_node).next = NULL;
+
+    if (!*head)
+        *head = last_node;
+    else
+    {
+        curr_node = *head;
+        while ((*curr_node).next)
+            curr_node = (*curr_node).next;
+        (*curr_node).next = last_node;
+    }
+    return (last_node);
+}
