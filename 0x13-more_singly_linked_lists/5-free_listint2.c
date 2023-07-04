45da7e704ec5733dabda49d07e5ff7d10df14389
El-gibbor <chiagoziem11@yahoo.com>
2023-07-04 14:32:24 +0400

    frees a single list whose address to the head ptr is passed as arg

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: address of the head pointer to the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *listFree;
+
+	while (*head)
+	{
+		listFree = *head;
+		*head = (*head)->next;
+		free(listFree);
+	}
+}
