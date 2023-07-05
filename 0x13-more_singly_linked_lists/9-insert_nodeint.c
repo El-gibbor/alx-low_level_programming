d96125f8557eec324ed1a8d07aaa9cc10b1d3b38
El-gibbor <chiagoziem11@yahoo.com>
2023-07-05 14:05:12 +0400

    refactored to handle impossible insertion at index (maybe, index out of bound)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,24 +7,37 @@
  * @n: node/value to be inserted
  * Return: address of the new node, or NULL if it failed
  */
+#include <stdlib.h>
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *curr_node, *node_at_indx = malloc(sizeof(listint_t));
+	listint_t *curr_node, *node_at_indx;
 
+	node_at_indx = malloc(sizeof(listint_t));
 	if (!node_at_indx)
 		return (NULL);
 
 	node_at_indx->n = n;
 
-	if (!*head || idx <= 0)
+	if (idx == 0 || !*head)
 	{
 		node_at_indx->next = *head;
 		*head = node_at_indx;
 		return (node_at_indx);
 	}
+
 	curr_node = *head;
-	while (curr_node->next && --idx)/*if curr_node isnt NULL and idx is non-zero*/
+	while (idx > 1 && curr_node)
+	{
 		curr_node = curr_node->next;
+		idx--;
+	}
+
+	if (!curr_node)
+	{
+		free(node_at_indx);
+		return (NULL);
+	}
 
 	node_at_indx->next = curr_node->next;
 	curr_node->next = node_at_indx;
