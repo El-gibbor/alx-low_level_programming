4c6ad390524e31486cb3da5bc141a02b6f761425
El-gibbor <chiagoziem11@yahoo.com>
2023-07-11 15:03:09 +0400

    cretes file and write to itls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,15 +12,16 @@ int create_file(const char *filename, char *text_content)
 
     if (!filename)
         return (-1);
+
+    newFile = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (newFile == -1)
+		return (-1);
+    
 	if (!text_content)
 		text_content = "";
 
-	while (text_content && text_content[len]) /* length of the string */
+	while (text_content[len]) /* length of the string */
 		len++;
-
-	newFile = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-	if (newFile == -1)
-		return (-1);
 
 	writeNewFile = write(newFile, text_content, len);
 	if (writeNewFile == -1)
