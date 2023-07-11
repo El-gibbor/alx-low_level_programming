c59d7f6a47d3481920a3524af6d8e0c2b2f1dbdf
El-gibbor <chiagoziem11@yahoo.com>
2023-07-11 15:07:24 +0400

    cretes file and write to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int writeNewFile, len, newFile;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    newFile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	newFile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (newFile == -1)
 		return (-1);
-    
+
 	if (!text_content)
 		text_content = "";
 
