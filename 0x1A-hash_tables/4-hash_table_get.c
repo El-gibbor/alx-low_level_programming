06490c09e37f9fefb19601b91573c1e1c30c0c7d
El-gibbor <chiagoziem11@yahoo.com>
2023-09-13 15:02:00 +0400

    func that  retrieves a value associated with a key.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * my_hash_table_get - Retrieve a hash table value based on its key.
+ * @table: The hash table to search.
+ * @key: The key of the valueto be retrieved
+ * Return: value associated with the specified key if found; otherwise, NULL.
+ */
+char *hash_table_get(const hash_table_t *table, const char *key)
+{
+	hash_node_t *current_node = NULL;
+	unsigned long int index = 0;
+
+	if (!table || !key || !key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, table->size);
+
+	current_node = table->array[index];
+	while (current_node)
+	{
+		if (strcmp(key, current_node->key) == 0)
+			return (current_node->value);
+
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
