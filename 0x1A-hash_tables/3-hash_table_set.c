c09fc1cbda1d818e9dee0e421b5a99a158b6b34c
El-gibbor <chiagoziem11@yahoo.com>
2023-09-13 15:48:18 +0400

    Improved impelementation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,47 +1,37 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table to add or update the key/value to.
- * @key: The key to add to the hash table.
- * @value: The value associated with the key.
- * Return: 1 if successful, 0 otherwise.
+ * hash_table_set - Adds an element into a hash table.
+ * @table: The hash table into which we're inserting an element.
+ * @key: The key of the hash node.
+ * @value: The value of the hash node.
+ *
+ * Return: Returns 1 for success and 0 for failure.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int hash_table_set(hash_table_t *table, const char *key, const char *value)
 {
-	hash_node_t *new_node, *tmp_ptr;
-	unsigned long int index;
+	unsigned long int index = 0;
+	hash_node_t *new_node;
 
-	if (!ht || !key || !*key || !value)
-		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-	tmp_ptr = ht->array[index];
-	while (tmp_ptr)
-	{
-		if (strcmp(tmp_ptr->key, key) == 0)
-		{
-			free(tmp_ptr->value);
-			tmp_ptr->value = strdup(value);
-			if (!tmp_ptr->value)
-				return (0);
-			return (1);
-		}
-		tmp_ptr = tmp_ptr->next;
-	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
+		return (NULL);
+
+	if (!table || !key)
 		return (0);
-	new_node->key = strdup(key);
+
+	index = key_index((const unsigned char *)key, table->size);
 	new_node->value = strdup(value);
-	if (!new_node->key || !new_node->value)
+	new_node->key = strdup(key);
+	new_node->next = NULL;
+
+	if (!table->array[index])
+		table->array[index] = new_node;
+	else
 	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-		return (0);
+		hash_node_t *current_node = table->array[index];
+		new_node->next = current_node;
+		table->array[index] = new_node;
 	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
 	return (1);
 }
