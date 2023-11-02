7d89d46eaeb639def538820a2678b29171708c11
El-gibbor <chiagoziem11@yahoo.com>
2023-11-02 23:26:06 +0400

    returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-"""This Module holds methods that calculate the perimeter of
-an island in a grid.
-"""
+'''
+Contains the function island_perimeter
+'''
 
-
-def count_water_neighbors(grid, i, j):
-    """Count the number of water neighbors a cell has in the grid."""
-    num = 0
-    if i > 0 and not grid[i - 1][j]:
-        num += 1
-    if i < len(grid) - 1 and not grid[i + 1][j]:
-        num += 1
-    if j > 0 and not grid[i][j - 1]:
-        num += 1
-    if j < len(grid[i]) - 1 and not grid[i][j + 1]:
-        num += 1
-    return num
 
 def island_perimeter(grid):
-    """Calculate the perimeter of the island in the grid."""
-    param = 0
+    '''
+    Returns the perimeter of the island descriibed in a grid
+    Args:
+        grid (list): A list of lists of integers
+    '''
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j]:
-                param += count_water_neighbors(grid, i, j)
-    return param
+            if grid[i][j] == 1:
+                try:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                except:
+                    pass
+
+                try:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                except:
+                    pass
+
+                try:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except:
+                    pass
+
+                try:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                except:
+                    pass
+
+    return perimeter
